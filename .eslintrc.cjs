6079229d91b5c3e248d6340f213ac11967ca7418
csh1314 <cshsb@qq.com>
2024-09-19 14:35:17 +0800

    feat: init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint', 'no-only-tests'],
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
